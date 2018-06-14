1132ceaadba00925e17f556b25981f3ccd388ee2
Tom <demto13@gmail.com>
2018-06-14 20:51:35 +0100

    Added Admin and Main screens

---
+++ UserDataStoreApp/Client/MAinScreen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserDataStoreApp.BusinessLogic.ServiceLogic.Services;
+
+namespace UserDataStoreApp.Client
+{
+    public class MainScreen
+    {
+
+        public static void RunMainScreen()
+        {
+            var userServices = new UserServices();
+            int userSelection = 0;
+
+            while (userSelection != 3)
+            {
+                Console.Clear();
+
+                Console.WriteLine("\n\nPlease select an opption:\n1.Admin\n2.Shop\n3.Exit\n ");
+                try
+                {
+                    userSelection = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Sorry, could not understand selection as : " + e.Message);
+                }
+
+                Console.Clear();
+
+                switch(userSelection){
+                    case 1:
+                        Console.WriteLine("Are you an admin? (y or n)");
+                        string isAdmin = Console.ReadLine();
+                        if(!string.Equals(isAdmin.ToLower(), "y")){
+                            Console.WriteLine("Sorry, this option is for admins only. Please press any key to continue.");
+                            Console.ReadLine();
+                            break;
+                        }else{
+                            Console.Clear();
+                            AdminScreen.RunAdminScreen();
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Please enter your name: ");
+                        string userName = Console.ReadLine();
+                        if(userServices.GetUser(userName) == null){
+                            Console.WriteLine("Sorry, you need to be added to the shop by an admin.");
+                            break;
+                        }else{
+                             // Shopscreen.RunShopScreen();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, invalid selection.");
+                        break;
+                }
+            }
+
+        }
+    }
+}
--- UserDataStoreApp/Program.cs
+++ UserDataStoreApp/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UserDataStoreApp.BusinessLogic.Domain;
-using UserDataStoreApp.BusinessLogic.ServiceLogic.Services;
+﻿using UserDataStoreApp.Client;
 
 namespace UserDataStoreApp
 {
@@ -12,102 +6,7 @@ namespace UserDataStoreApp
     {
         static void Main(string[] args)
         {
-            Run();
-        }
-
-        public static void Run(){
-            UserServices userServices = new UserServices();
-            int userSelection = 0;
-
-            while (userSelection != 5)
-            {
-                Console.WriteLine("\n\nPlease select from the options below: " +
-                                    "\n\n1. View Users in Db\n2. Update a user's name\n3. Add new User\n4. Delete User\n5. Exit");
-                try
-                {
-                    userSelection = int.Parse(Console.ReadLine());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Could not read answer as: " + e.Message + "\nPlease try again");
-                }
-
-                switch (userSelection)
-                {
-                    case 1:
-                        foreach (var user in userServices.GetAllUsersFromDb())
-                        {
-                            Console.WriteLine($"{user.UserId}\n{user.UserName}\n{user.NickName}\n{user.IsAdmin}\n\n");
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Please give me the name of the user: ");
-                        string userName = Console.ReadLine();
-                        var user2update = userServices.GetUser(userName);
-                        Console.WriteLine(user2update == null ? "Sorry, user does not exist in db" : "What would you like to change the name to: ");
-                        if (user2update != null)
-                        {
-                            var newName = Console.ReadLine();
-
-                            if (userServices.UpdateUserName(user2update.UserId, newName))
-                            {
-                                Console.WriteLine("User's name is succesfully updated.");
-                            };
-                            break;
-                        }
-                        Console.WriteLine("Sorry, something went wrong with the update");
-                        break;
-                    case 3:
-                        var newUser = new User();
-                        Console.WriteLine("Please enter the new user's Name: ");
-                        newUser.UserName = Console.ReadLine();
-                        Console.WriteLine("Please enter a nick name: ");
-                        newUser.NickName = Console.ReadLine();
-                        Console.WriteLine("Please enter y or n to indicate if the user is an admin: ");
-                        if (string.Equals(Console.ReadLine().ToLower(), "y"))
-                        {
-                            newUser.IsAdmin = true;
-                        }
-                        else
-                        {
-                            newUser.IsAdmin = false;
-                        };
-                        if (userServices.SaveUser(newUser))
-                        {
-                            Console.WriteLine("New User saved successfully");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Could not save new user");
-                        }
-                        break;
-                    case 4:
-                        Console.WriteLine("Please enter the user id for the user to be deleted: ");
-                        int id = 0;
-
-                        try{
-                            id = int.Parse(Console.ReadLine());
-                        }catch(Exception e){
-                            Console.WriteLine("Sorry, incorrect input: " + e.Message);
-                            break;
-                        }
-
-                        if (userServices.DeleteUser(id))
-                        {
-                            Console.WriteLine("User successfully deleted");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Could not delete user");
-                        }
-                        break;
-                    case 5:
-                        break;
-                    default:
-                        Console.WriteLine("Sorry, incorrect option");
-                        break;
-                }
-            }
+            MainScreen.RunMainScreen();
         }
     }
 }
