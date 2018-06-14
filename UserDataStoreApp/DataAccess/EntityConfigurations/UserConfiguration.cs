bb8cf8e80a5f98ca9aca683cf226958ce4a6fd2e
Tom <demto13@gmail.com>
2018-06-14 19:29:54 +0100

    Added UserConfiguration

---
+++ UserDataStoreApp/DataAccess/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserDataStoreApp.BusinessLogic.Domain;
+
+namespace UserDataStoreApp.DataAccess.EntityConfigurations
+{
+    public class UserConfiguration : EntityTypeConfiguration<User>
+    {
+        public UserConfiguration()
+        {
+            // Table overrides
+
+            // Keys
+
+            // Properties
+
+            Property(p => p.NickName)
+            .HasColumnOrder(2);
+
+            Property(p => p.UserId)
+            .HasColumnName("UserId");
+
+            Property(p => p.UserName)
+            .IsRequired()
+            .HasMaxLength(50);
+
+            Property(p => p.NickName)
+            .IsRequired()
+            .HasMaxLength(30);
+
+            Property(p => p.IsAdmin)
+            .IsRequired();
+
+            // Relationships
+        }
+    }
+}
