ae652ce20899bf2cfe803cb3850bd34127e46701
Tom <demto13@gmail.com>
2018-06-21 20:59:56 +0100

    Added ILogger and SeverityLevel enum

---
+++ UserDataStoreApp/Shared/Enums/SeverityLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDataStoreApp.Shared.Enums
+{
+    public enum SeverityLevel
+    {
+        Fatal = 0,
+        Error = 1,
+        Warning = 2,
+        Verbose = 3
+    }
+}
---
+++ UserDataStoreApp/Shared/Services/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserDataStoreApp.Shared.Enums;
+
+namespace UserDataStoreApp.Shared.Services
+{
+    public interface ILogger
+    {
+        void TraceError(string title, string message, SeverityLevel severityLevel);
+    }
+}
